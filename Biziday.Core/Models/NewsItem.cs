de225a3199097535cedf29db8b54e4e9a4c7abbf
Bogdan Bujdea <bogdan@thewindev.net>
2017-01-19 01:53:50 +0200

    Modified news background by news type

--- Biziday.Core/Models/NewsItem.cs
+++ Biziday.Core/Models/NewsItem.cs
@@ -1,4 +1,6 @@
-﻿namespace Biziday.Core.Models
+﻿using Biziday.Core.Modules.News.Models;
+
+namespace Biziday.Core.Models
 {
     public class NewsItem
     {
@@ -7,7 +9,7 @@
         public string Body { get; set; }
         public string Link { get; set; }
         public int CategoryId { get; set; }
-        public int NewsType { get; set; }
+        public NewsType NewsType { get; set; }
         public int EditorGrade { get; set; }
         public string SourceUrl { get; set; }
         public int PublishedOn { get; set; }
